3040986bb340aa4d9c60195b708d83e50bc38c1c
Kamalakar-shetake <104167519+Kamalakar-shetake@users.noreply.github.com>
2022-04-22 22:55:34 +0530

    Delete talend directory

--- talend/tests/tes_tuple_sort.py
+++
@@ -1,7 +0,0 @@
-from src import tuple_sort
-
-def test_tuple_sort(tuple_list):
-    """Test case to validate insertion of data into BST"""
-    actual_output = tuple_sort.get_output(tuple_list)
-    expected_output = [('sop', '43', '56'), ('tom', '24', '26'), ('tom', '5', '24'), ('tor', '2', '8')]
-    assert expected_output == actual_output
